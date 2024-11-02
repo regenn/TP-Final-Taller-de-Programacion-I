91edcf5f79eb8e1aeb2af642363a7601b6544a8c
regenn <reginaentrocassi@gmail.com>
2024-11-02 18:11:17 -0300

    avances empresa
    escenario 2

--- src/test/EmpresaTest.java
+++ src/test/EmpresaTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 import modeloDatos.*;
+//import Utils.MockUtils;
 import excepciones.*;
 import modeloNegocio.*;
 import java.util.HashMap;
@@ -12,13 +13,290 @@ import java.util.ArrayList;
 
 //escenario
 public class EmpresaTest {
-    Empresa empresa; 
-    Chofer chofer;
-    Vehiculo auto;
-    Vehiculo moto;
-    Vehiculo combi;
+    Empresa empresa;
+
+    Chofer choferTemp,choferPerm;
+    Cliente cliente;
+    Vehiculo auto,moto,combi;
+
+    HashMap<String,Cliente>clientes;
+    HashMap<String,Chofer>choferes;
+    HashMap<String,Vehiculo>vehiculos;
     
-    //setup de una empresa vacia -> instancia=Empresa.getInstance()
+    @Before
+    public void setUp(){
+
+        clientes=new HashMap<String,Cliente>();
+        choferes=new HashMap<String,Chofer>();
+        vehiculos=new HashMap<String,Vehiculo>();
+
+        empresa = Empresa.getInstance();
+        choferTemp = new ChoferTemporario("12345","nombreChofer");
+        choferPerm = new ChoferPermanente("12346","nombreChofer2", 2023, 2);
+
+        choferes.put("12345",choferTemp);
+        choferes.put("12346",choferPerm);
+
+        empresa.setChoferes(choferes);
+
+        moto = new Moto("123-456");
+        auto = new Auto("234-567", 3, true);
+        combi = new Combi("345-678", 6, false);
+
+        vehiculos.put("123-456",moto);
+        vehiculos.put("234-567",auto);
+        vehiculos.put("345-678",combi);
+
+        empresa.setVehiculos(vehiculos);
+
+        cliente=new Cliente("usuario","pass","nombreCliente");
+        clientes.put("usuario",cliente);
+        
+        empresa.setClientes(clientes);
+    
+        /*try{
+            empresa.agregarChofer(choferPerm);
+            empresa.agregarChofer(choferTemp);
+            empresa.agregarVehiculo(moto);
+            empresa.agregarVehiculo(auto);
+            empresa.agregarVehiculo(combi);
+            empresa.agregarCliente("usuario","pass","nombreCliente");
+        }
+        catch(Exception ex){
+            fail("No deberia tirar una excepcion");
+        }*/
+    }
+    
+
+    
+    @After
+    public void tearDown(){
+        //TODO: limpiar la empresa.
+        
+    }
+    @Test
+    public void agregarChoferTest(){
+        Chofer chofernuevo=new ChoferTemporario("7882345","nombreEjemplo");
+        try{
+            empresa.agregarChofer(chofernuevo);
+        }
+        catch(Exception ex){
+            fail("Se lanzo una excepcion no esperada");
+        }
+        assertEquals(chofernuevo,empresa.getChoferes().get("7882345"));
+        try{
+            empresa.agregarChofer(chofernuevo);
+        }
+        catch(ChoferRepetidoException ex){
+            assertEquals(chofernuevo,ex.getChoferExistente());
+        }
+        catch(Exception ex){
+            fail("Se lanzo una excepcion no esperada");
+        }
+    }
+
+    @Test
+    public void agregarClienteTest(){
+        try{
+            empresa.agregarCliente("usuario1","pass1","nombreEjemplo");
+        }
+        catch(Exception ex){
+            fail("Se lanzo una excepcion no esperada");
+        }
+        assertEquals("usuario1",empresa.getClientes().get("usuario1").getNombreUsuario());
+        try{
+            empresa.agregarCliente("usuario1","pass1","nombreEjemplo");
+        }
+        catch(UsuarioYaExisteException ex){
+            assertEquals(ex.getUsuarioPretendido(),empresa.getClientes().get("usuario1"));
+        }
+        catch(Exception ex){
+            fail("Se lanzo una excepcion no esperada");
+        }
+    }
+
+    @Test
+    public void agregarVehiculoTest(){
+        Vehiculo vehiculonuevo=new Auto("453524",3,true);
+        try{
+            empresa.agregarVehiculo(vehiculonuevo);
+        }
+        catch(Exception ex){
+            fail("Se lanzo una excepcion no esperada");
+        }
+        //TODO: fijarse que los valores de testeo sean iguales a los del excel!!!!1
+        assertEquals(vehiculonuevo,empresa.getVehiculos().get("453524"));
+        try{
+            empresa.agregarVehiculo(vehiculonuevo);
+        }
+        catch(VehiculoRepetidoException ex){
+            assertEquals(vehiculonuevo,ex.getVehiculoExistente());
+        }
+        catch(Exception ex){
+            fail("Se lanzo una excepcion no esperada");
+        }
+    }
+
+    @Test
+    public void getTotalSalariosTest(){
+        assertEquals(empresa.getTotalSalarios(),(choferPerm.getSueldoBruto()+choferTemp.getSueldoBruto()),0.001);
+    }
+    
+    @Test
+    public void loginTest1(){
+        try{
+            Usuario u = empresa.login("admin", "1234");   
+            empresa.logout();    
+        }
+        catch(PasswordErroneaException e){}//error esperado
+        catch(UsuarioNoExisteException e){}
+    }
+
+    @Test
+    public void loginTest2(){
+        try{
+            Usuario u = empresa.login("Paulmescal", "1234"); 
+            empresa.logout();      
+        }
+        catch(PasswordErroneaException e){}
+        catch(UsuarioNoExisteException e){}//error esperado
+    }
+    
+    @Test
+    public void loginTest3(){
+        try{
+            Usuario u = empresa.login("admin", "admin");
+            assertEquals(u, empresa.getUsuarioLogeado());
+            empresa.logout();       
+        }
+        catch(PasswordErroneaException e){}
+        catch(UsuarioNoExisteException e){}
+    }    
+
+    @Test
+    public void logoutTest1(){
+        try{
+            Usuario u = empresa.login("admin", "admin");
+            empresa.logout();
+            assertEquals(null, empresa.getUsuarioLogeado());
+        }
+        catch(PasswordErroneaException e){}
+        catch(UsuarioNoExisteException e){}
+    }  
+    
+    @Test
+    public void isAdminTest1(){
+        try{
+            Usuario u = empresa.login("admin", "admin");
+            assertEquals(true, empresa.isAdmin());
+            empresa.logout();
+        }
+        catch(PasswordErroneaException e){}
+        catch(UsuarioNoExisteException e){}
+    }       
+    
+    @Test
+    public void setUsuarioLogeadoTest(){
+        try{
+            Usuario u= new Cliente("usuario2","pass2","nombre2");
+            empresa.setUsuarioLogeado(u);
+            assertEquals(u,empresa.getUsuarioLogeado());
+            empresa.logout();            
+        }
+        catch(Exception ex){
+            fail("Se lanzo una excepcion no esperada");
+        }
+    }
+    
+    @Test
+    public void getUsuarioLogeadoTest1(){
+        try{
+            Usuario u = empresa.login("admin", "admin");
+            assertEquals(u, empresa.getUsuarioLogeado());
+            empresa.logout();
+        }
+        catch(PasswordErroneaException e){}
+        catch(UsuarioNoExisteException e){}
+    }  
+
+    @Test
+    public void getUsuarioLogeadoTest2(){
+        assertEquals(null, empresa.getUsuarioLogeado());
+    }        
+
+    @Test
+    public void setVehiculosTest(){
+        HashMap<String,Vehiculo> vehiculos = new HashMap<String,Vehiculo>();
+        empresa.setVehiculos(vehiculos);
+        assertEquals(vehiculos, empresa.getVehiculos());
+    }    
+
+    @Test 
+    public void getChoferesTest(){
+       assertEquals(empresa.getChoferes(),choferes);
+    }
+
+    @Test 
+    public void getChoferesDesocupadosTest(){
+       assertEquals(2,empresa.getChoferesDesocupados().size());
+       assertEquals(true,empresa.getChoferesDesocupados().contains(choferPerm));
+       assertEquals(true,empresa.getChoferesDesocupados().contains(choferTemp));
+    }
+
+    @Test 
+    public void getClientesTest(){
+      assertEquals(empresa.getClientes(),clientes);
+    }
+
+    @Test
+    public void getVehiculosTest(){
+        assertEquals(vehiculos, empresa.getVehiculos());
+    }         
+
+    @Test
+    public void getVehiculosDesocupadosTest(){
+        assertEquals(3, empresa.getVehiculos().size());
+        assertEquals(true, empresa.getVehiculosDesocupados().contains(moto));
+        assertEquals(true, empresa.getVehiculosDesocupados().contains(auto));
+        assertEquals(true, empresa.getVehiculosDesocupados().contains(combi));
+    }      
+    
+    @Test
+    public void setVehiculosDesocupadosTest(){
+        ArrayList<Vehiculo> vehiculosDesocupados= new ArrayList<Vehiculo>();
+        empresa.setVehiculosDesocupados(vehiculosDesocupados);
+        assertEquals(vehiculosDesocupados,empresa.getVehiculosDesocupados());
+
+    }
+
+    @Test
+    public void setChoferesTest(){
+        HashMap<String,Chofer> choferes= new HashMap<String,Chofer>();
+        empresa.setChoferes(choferes);
+        assertEquals(choferes,empresa.getChoferes());
+    }
+
+    @Test
+    public void setChoferesDesocupadosTest(){
+        ArrayList<Chofer> choferesDesocupados= new ArrayList<Chofer>();
+        empresa.setChoferesDesocupados(choferesDesocupados);
+        assertEquals(choferesDesocupados,empresa.getChoferesDesocupados());
+    
+    }
+    
+    @Test
+    public void setClientesTest(){
+        HashMap<String,Cliente> clientes= new HashMap<String,Cliente>();
+        empresa.setClientes(clientes);
+     
+   assertEquals(clientes,empresa.getChoferesDesocupados());
+    }
+    /*
+    
+    ##########################################################################################
+    ##########################################################################################
+    
+    
     @Before
     public void setUp() throws Exception{
         empresa = Empresa.getInstance();
@@ -100,7 +378,6 @@ public class EmpresaTest {
 ClienteNoExisteException,
 ClienteConViajePendienteException,
 ClienteConPedidoPendienteException
-        */
       
     }
 
