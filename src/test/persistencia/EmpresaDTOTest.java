5642768db010cabb2f66f97da910167a4e5cdcd6
regenn <reginaentrocassi@gmail.com>
2024-11-05 21:08:36 -0300

    EmpresaDTO :3

--- src/test/persistencia/EmpresaDTOTest.java
+++ src/test/persistencia/EmpresaDTOTest.java
@@ -40,15 +40,29 @@ public class EmpresaDTOTest {
 
             ArrayList<Chofer> choferesDesocupadosTest = new ArrayList<Chofer>();
             ArrayList<Viaje> viajesTerminadosTest = new ArrayList<Viaje>();
+            ArrayList<Vehiculo>vehiculosDesocupadosTest = new ArrayList<Vehiculo>();
             
             Cliente cliente= new Cliente("usuarioTest","passTest","nombreTest");
             clientesTest.put("usuarioTest",cliente);
 
             Chofer chofer= new ChoferTemporario("12345","nombreChoferTest");
             choferesTest.put("12345",chofer);
+            choferesDesocupadosTest.add(chofer);
+
+            Usuario admin= Administrador.getInstance();
 
             empresaDTO.setChoferes(choferesTest);
-
+            empresaDTO.setChoferesDesocupados(choferesDesocupadosTest);
+            empresaDTO.setClientes(clientesTest);
+            empresaDTO.setPedidos(pedidosTest);
+            empresaDTO.setUsuarioLogeado(admin);
+            empresaDTO.setVehiculos(vehiculosTest);
+            empresaDTO.setVehiculosDesocupados(vehiculosDesocupadosTest);
+            empresaDTO.setViajesIniciados(viajesIniciadosTest);
+            empresaDTO.setViajesTerminados(viajesTerminadosTest);
+        }
+        catch(Exception ex){
+            fail("Se lanzo una excepcion no esperada");
         }
     }
 }
